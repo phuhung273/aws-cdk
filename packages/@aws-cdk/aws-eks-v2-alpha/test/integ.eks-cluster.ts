d88882364d34b940b1b4e7e0258558524d7e7fb4
phuhung273 <phuhung.tranpham@gmail.com>
2025-06-06 17:29:31 +0000

    init

--- packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-cluster.ts
+++ packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-cluster.ts
@@ -1,15 +1,10 @@
 /// !cdk-integ pragma:disable-update-workflow
-import * as path from 'path';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as kms from 'aws-cdk-lib/aws-kms';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-import { App, CfnOutput, Duration, Token, Fn, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, Token, Fn, Stack, StackProps } from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
-import * as cdk8s from 'cdk8s';
-import * as kplus from 'cdk8s-plus-27';
-import * as constructs from 'constructs';
-import { KubectlV32Layer } from '@aws-cdk/lambda-layer-kubectl-v32';
+import { KubectlV33Layer } from '@aws-cdk/lambda-layer-kubectl-v33';
 import * as hello from './hello-k8s';
 import * as eks from '../lib';
 import { IAM_OIDC_REJECT_UNAUTHORIZED_CONNECTIONS } from 'aws-cdk-lib/cx-api';
@@ -41,10 +36,11 @@ class EksClusterStack extends Stack {
     this.cluster = new eks.Cluster(this, 'Cluster', {
       vpc: this.vpc,
       vpcSubnets,
+      serviceIpv4Cidr: '172.20.0.0/16',
       mastersRole,
       defaultCapacityType: eks.DefaultCapacityType.NODEGROUP,
       defaultCapacity: 2,
-      version: eks.KubernetesVersion.V1_32,
+      version: eks.KubernetesVersion.V1_33,
       secretsEncryptionKey,
       tags: {
         foo: 'bar',
@@ -55,7 +51,7 @@ class EksClusterStack extends Stack {
         eks.ClusterLoggingTypes.SCHEDULER,
       ],
       kubectlProviderOptions: {
-        kubectlLayer: new KubectlV32Layer(this, 'kubectlLayer'),
+        kubectlLayer: new KubectlV33Layer(this, 'kubectlLayer'),
       },
     });
 
@@ -65,37 +61,37 @@ class EksClusterStack extends Stack {
 
     this.assertCapacityArm();
 
-    this.assertBottlerocket();
+    // this.assertBottlerocket();
 
-    this.assertSpotCapacity();
+    // this.assertSpotCapacity();
 
     this.assertNodeGroupX86();
 
-    this.assertNodeGroupSpot();
+    // this.assertNodeGroupSpot();
 
-    this.assertNodeGroupArm();
+    // this.assertNodeGroupArm();
 
     this.assertNodeGroupGraviton3();
 
     this.assertNodeGroupCustomAmi();
 
-    this.assertNodeGroupGpu();
+    // this.assertNodeGroupGpu();
 
     this.assertSimpleManifest();
 
-    this.assertManifestWithoutValidation();
+    // this.assertManifestWithoutValidation();
 
-    this.assertSimpleHelmChart();
+    // this.assertSimpleHelmChart();
 
-    this.assertHelmChartAsset();
+    // this.assertHelmChartAsset();
 
-    this.assertSimpleCdk8sChart();
+    // this.assertSimpleCdk8sChart();
 
-    this.assertCreateNamespace();
+    // this.assertCreateNamespace();
 
-    this.assertServiceAccount();
+    // this.assertServiceAccount();
 
-    this.assertExtendedServiceAccount();
+    // this.assertExtendedServiceAccount();
 
     new CfnOutput(this, 'ClusterEndpoint', { value: this.cluster.clusterEndpoint });
     new CfnOutput(this, 'ClusterArn', { value: this.cluster.clusterArn });
@@ -106,115 +102,116 @@ class EksClusterStack extends Stack {
     new CfnOutput(this, 'NodegroupName', { value: this.cluster.defaultNodegroup!.nodegroupName });
   }
 
-  private assertServiceAccount() {
-    // add a service account connected to a IAM role
-    this.cluster.addServiceAccount('MyServiceAccount');
-  }
+  // private assertServiceAccount() {
+  //   // add a service account connected to a IAM role
+  //   this.cluster.addServiceAccount('MyServiceAccount');
+  // }
 
-  private assertExtendedServiceAccount() {
-    // add a service account connected to a IAM role
-    this.cluster.addServiceAccount('MyExtendedServiceAccount', {
-      annotations: {
-        'eks.amazonaws.com/sts-regional-endpoints': 'false',
-      },
-      labels: {
-        'some-label': 'with-some-value',
-      },
-    });
-  }
+  // private assertExtendedServiceAccount() {
+  //   // add a service account connected to a IAM role
+  //   this.cluster.addServiceAccount('MyExtendedServiceAccount', {
+  //     annotations: {
+  //       'eks.amazonaws.com/sts-regional-endpoints': 'false',
+  //     },
+  //     labels: {
+  //       'some-label': 'with-some-value',
+  //     },
+  //   });
+  // }
 
-  private assertCreateNamespace() {
-    // deploy an nginx ingress in a namespace
-    const nginxNamespace = this.cluster.addManifest('nginx-namespace', {
-      apiVersion: 'v1',
-      kind: 'Namespace',
-      metadata: {
-        name: 'nginx',
-      },
-    });
+  // private assertCreateNamespace() {
+  //   // deploy an nginx ingress in a namespace
+  //   const nginxNamespace = this.cluster.addManifest('nginx-namespace', {
+  //     apiVersion: 'v1',
+  //     kind: 'Namespace',
+  //     metadata: {
+  //       name: 'nginx',
+  //     },
+  //   });
 
-    const nginxIngress = this.cluster.addHelmChart('nginx-ingress', {
-      chart: 'nginx-ingress',
-      repository: 'https://helm.nginx.com/stable',
-      namespace: 'nginx',
-      wait: true,
-      release: 'nginx-ingress',
-      // https://github.com/nginxinc/helm-charts/tree/master/stable
-      version: '0.17.1',
-      values: {
-        controller: {
-          service: {
-            create: false,
-          },
-        },
-      },
-      createNamespace: false,
-      timeout: Duration.minutes(15),
-    });
+  //   const nginxIngress = this.cluster.addHelmChart('nginx-ingress', {
+  //     chart: 'nginx-ingress',
+  //     repository: 'https://helm.nginx.com/stable',
+  //     namespace: 'nginx',
+  //     wait: true,
+  //     release: 'nginx-ingress',
+  //     // https://github.com/nginxinc/helm-charts/tree/master/stable
+  //     version: '0.17.1',
+  //     values: {
+  //       controller: {
+  //         service: {
+  //           create: false,
+  //         },
+  //       },
+  //     },
+  //     createNamespace: false,
+  //     timeout: Duration.minutes(15),
+  //   });
 
-    // make sure namespace is deployed before the chart
-    nginxIngress.node.addDependency(nginxNamespace);
-  }
+  //   // make sure namespace is deployed before the chart
+  //   nginxIngress.node.addDependency(nginxNamespace);
+  // }
 
-  private assertSimpleCdk8sChart() {
-    class Chart extends cdk8s.Chart {
-      constructor(scope: constructs.Construct, ns: string, cluster: eks.ICluster) {
-        super(scope, ns);
+  // private assertSimpleCdk8sChart() {
+  //   class Chart extends cdk8s.Chart {
+  //     constructor(scope: constructs.Construct, ns: string, cluster: eks.ICluster) {
+  //       super(scope, ns);
 
-        new kplus.ConfigMap(this, 'config-map', {
-          data: {
-            clusterName: cluster.clusterName,
-          },
-        });
-      }
-    }
-    const app = new cdk8s.App();
-    const chart = new Chart(app, 'Chart', this.cluster);
+  //       new kplus.ConfigMap(this, 'config-map', {
+  //         data: {
+  //           clusterName: cluster.clusterName,
+  //         },
+  //       });
+  //     }
+  //   }
+  //   const app = new cdk8s.App();
+  //   const chart = new Chart(app, 'Chart', this.cluster);
 
-    this.cluster.addCdk8sChart('cdk8s-chart', chart);
-  }
-  private assertSimpleHelmChart() {
-    // deploy the Kubernetes dashboard through a helm chart
-    this.cluster.addHelmChart('dashboard', {
-      chart: 'kubernetes-dashboard',
-      // https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
-      version: '6.0.8',
-      repository: 'https://kubernetes.github.io/dashboard/',
-    });
-  }
+  //   this.cluster.addCdk8sChart('cdk8s-chart', chart);
+  // }
+  // private assertSimpleHelmChart() {
+  //   // deploy the Kubernetes dashboard through a helm chart
+  //   this.cluster.addHelmChart('dashboard', {
+  //     chart: 'kubernetes-dashboard',
+  //     // https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
+  //     version: '6.0.8',
+  //     repository: 'https://kubernetes.github.io/dashboard/',
+  //   });
+  // }
 
-  private assertHelmChartAsset() {
-    // get helm chart from Asset
-    const chartAsset = new Asset(this, 'ChartAsset', {
-      path: path.join(__dirname, 'test-chart'),
-    });
-    this.cluster.addHelmChart('test-chart', {
-      chartAsset: chartAsset,
-    });
-  }
+  // private assertHelmChartAsset() {
+  //   // get helm chart from Asset
+  //   const chartAsset = new Asset(this, 'ChartAsset', {
+  //     path: path.join(__dirname, 'test-chart'),
+  //   });
+  //   this.cluster.addHelmChart('test-chart', {
+  //     chartAsset: chartAsset,
+  //   });
+  // }
 
   private assertSimpleManifest() {
-    // apply a kubernetes manifest
     this.cluster.addManifest('HelloApp', ...hello.resources);
   }
-  private assertManifestWithoutValidation() {
-    // apply a kubernetes manifest
-    new eks.KubernetesManifest(this, 'HelloAppWithoutValidation', {
-      cluster: this.cluster,
-      manifest: [{
-        apiVersion: 'v1',
-        kind: 'ConfigMap',
-        data: { hello: 'world' },
-        metadata: { name: 'config-map' },
-        unknown: { key: 'value' },
-      }],
-      skipValidation: true,
-    });
-  }
+
+  // private assertManifestWithoutValidation() {
+  //   // apply a kubernetes manifest
+  //   new eks.KubernetesManifest(this, 'HelloAppWithoutValidation', {
+  //     cluster: this.cluster,
+  //     manifest: [{
+  //       apiVersion: 'v1',
+  //       kind: 'ConfigMap',
+  //       data: { hello: 'world' },
+  //       metadata: { name: 'config-map' },
+  //       unknown: { key: 'value' },
+  //     }],
+  //     skipValidation: true,
+  //   });
+  // }
   private assertNodeGroupX86() {
     // add a extra nodegroup
     this.cluster.addNodegroupCapacity('extra-ng', {
       instanceTypes: [new ec2.InstanceType('t3.small')],
+      amiType: eks.NodegroupAmiType.AL2023_X86_64_STANDARD,
       minSize: 1,
       maxSize: 4,
       maxUnavailable: 3,
@@ -222,34 +219,40 @@ class EksClusterStack extends Stack {
       nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
     });
   }
-  private assertNodeGroupSpot() {
-    // add a extra nodegroup
-    this.cluster.addNodegroupCapacity('extra-ng-spot', {
-      instanceTypes: [
-        new ec2.InstanceType('c5.large'),
-        new ec2.InstanceType('c5a.large'),
-        new ec2.InstanceType('m7i-flex.large'),
-      ],
-      minSize: 3,
-      // reusing the default capacity nodegroup instance role when available
-      nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
-      capacityType: eks.CapacityType.SPOT,
-    });
-  }
+  // private assertNodeGroupSpot() {
+  //   // add a extra nodegroup
+  //   this.cluster.addNodegroupCapacity('extra-ng-spot', {
+  //     instanceTypes: [
+  //       new ec2.InstanceType('c5.large'),
+  //       new ec2.InstanceType('c5a.large'),
+  //       new ec2.InstanceType('m7i-flex.large'),
+  //     ],
+  //     amiType: eks.NodegroupAmiType.AL2023_X86_64_STANDARD,
+  //     minSize: 3,
+  //     // reusing the default capacity nodegroup instance role when available
+  //     nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
+  //     capacityType: eks.CapacityType.SPOT,
+  //   });
+  // }
   private assertNodeGroupCustomAmi() {
     // add a extra nodegroup
-    const userData = ec2.UserData.forLinux();
-    userData.addCommands(
-      'set -o xtrace',
-      `/etc/eks/bootstrap.sh ${this.cluster.clusterName}`,
-    );
+    const userData = `---
+apiVersion: node.eks.aws/v1alpha1
+kind: NodeConfig
+spec:
+  cluster:
+    name: ${this.cluster.clusterName}
+    apiServerEndpoint: ${this.cluster.clusterEndpoint}
+    certificateAuthority: ${this.cluster.clusterCertificateAuthorityData}
+    cidr: ${this.cluster.serviceIpv4Cidr}
+    `;
     const lt = new ec2.CfnLaunchTemplate(this, 'LaunchTemplate', {
       launchTemplateData: {
-        imageId: new eks.EksOptimizedImage({
-          kubernetesVersion: eks.KubernetesVersion.V1_25.version,
+        imageId: new eks.Eks2023OptimizedImage({
+          kubernetesVersion: eks.KubernetesVersion.V1_33.version,
         }).getImage(this).imageId,
         instanceType: new ec2.InstanceType('t3.small').toString(),
-        userData: Fn.base64(userData.render()),
+        userData: Fn.base64(userData),
       },
     });
     this.cluster.addNodegroupCapacity('extra-ng2', {
@@ -262,74 +265,79 @@ class EksClusterStack extends Stack {
       },
     });
   }
-  private assertNodeGroupArm() {
-    // add a extra nodegroup
-    this.cluster.addNodegroupCapacity('extra-ng-arm', {
-      instanceTypes: [new ec2.InstanceType('m6g.medium')],
-      minSize: 1,
-      maxUnavailablePercentage: 33,
-      // reusing the default capacity nodegroup instance role when available
-      nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
-    });
-  }
+  // private assertNodeGroupArm() {
+  //   // add a extra nodegroup
+  //   this.cluster.addNodegroupCapacity('extra-ng-arm', {
+  //     instanceTypes: [new ec2.InstanceType('m6g.medium')],
+  //     amiType: eks.NodegroupAmiType.AL2023_ARM_64_STANDARD,
+  //     minSize: 1,
+  //     maxUnavailablePercentage: 33,
+  //     // reusing the default capacity nodegroup instance role when available
+  //     nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
+  //   });
+  // }
   private assertNodeGroupGraviton3() {
     // add a Graviton3 nodegroup
     this.cluster.addNodegroupCapacity('extra-ng-arm3', {
       instanceTypes: [new ec2.InstanceType('c7g.large')],
+      amiType: eks.NodegroupAmiType.AL2023_ARM_64_STANDARD,
       minSize: 1,
       // reusing the default capacity nodegroup instance role when available
       nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
     });
   }
-  private assertNodeGroupGpu() {
-    // add a GPU nodegroup
-    this.cluster.addNodegroupCapacity('extra-ng-gpu', {
-      instanceTypes: [
-        new ec2.InstanceType('p2.xlarge'),
-        new ec2.InstanceType('g5.xlarge'),
-        new ec2.InstanceType('g6e.xlarge'),
-      ],
-      minSize: 1,
-      // reusing the default capacity nodegroup instance role when available
-      nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
-    });
-  }
-  private assertSpotCapacity() {
-    // spot instances (up to 10)
-    this.cluster.addAutoScalingGroupCapacity('spot', {
-      spotPrice: '0.1094',
-      instanceType: new ec2.InstanceType('t3.large'),
-      maxCapacity: 10,
-      bootstrapOptions: {
-        kubeletExtraArgs: '--node-labels foo=bar,goo=far',
-        awsApiRetryAttempts: 5,
-      },
-    });
-  }
-  private assertBottlerocket() {
-    // add bottlerocket nodes
-    this.cluster.addAutoScalingGroupCapacity('BottlerocketNodes', {
-      instanceType: new ec2.InstanceType('t3.small'),
-      minCapacity: 2,
-      machineImageType: eks.MachineImageType.BOTTLEROCKET,
-    });
-  }
+  // private assertNodeGroupGpu() {
+  //   // add a GPU nodegroup
+  //   this.cluster.addNodegroupCapacity('extra-ng-gpu', {
+  //     instanceTypes: [
+  //       new ec2.InstanceType('p2.xlarge'),
+  //       new ec2.InstanceType('g5.xlarge'),
+  //       new ec2.InstanceType('g6e.xlarge'),
+  //     ],
+  //     amiType: eks.NodegroupAmiType.AL2023_X86_64_NVIDIA,
+  //     minSize: 1,
+  //     // reusing the default capacity nodegroup instance role when available
+  //     nodeRole: this.cluster.defaultCapacity ? this.cluster.defaultCapacity.role : undefined,
+  //   });
+  // }
+  // private assertSpotCapacity() {
+  //   const asg = this.cluster.addAutoScalingGroupCapacity('spot', {
+  //     spotPrice: '0.1094',
+  //     instanceType: new ec2.InstanceType('t3.large'),
+  //     maxCapacity: 10,
+  //     machineImageType: eks.MachineImageType.AMAZON_LINUX_2023,
+  //   });
+
+  //   this.cluster.grantClusterAdmin('BottleRocketAccess', asg.role.roleArn);
+  // }
+  // private assertBottlerocket() {
+  //   const asg = this.cluster.addAutoScalingGroupCapacity('BottlerocketNodes', {
+  //     instanceType: new ec2.InstanceType('t3.small'),
+  //     minCapacity: 2,
+  //     machineImageType: eks.MachineImageType.BOTTLEROCKET,
+  //   });
+
+  //   this.cluster.grantClusterAdmin('BottleRocketAccess', asg.role.roleArn);
+  // }
   private assertCapacityX86() {
-    // add some x86_64 capacity to the cluster. The IAM instance role will
-    // automatically be mapped via aws-auth to allow nodes to join the cluster.
-    this.cluster.addAutoScalingGroupCapacity('Nodes', {
+    const asg = this.cluster.addAutoScalingGroupCapacity('Nodes', {
       instanceType: new ec2.InstanceType('t2.medium'),
       minCapacity: 3,
+      machineImageType: eks.MachineImageType.AMAZON_LINUX_2023,
     });
+
+    this.cluster.grantClusterAdmin('x86Access', asg.role.roleArn);
   }
 
   private assertCapacityArm() {
-    // add some arm64 capacity to the cluster. The IAM instance role will
-    // automatically be mapped via aws-auth to allow nodes to join the cluster.
-    this.cluster.addAutoScalingGroupCapacity('NodesArm', {
+    const asg = this.cluster.addAutoScalingGroupCapacity('NodesArm', {
       instanceType: new ec2.InstanceType('m6g.medium'),
       minCapacity: 1,
+      machineImageType: eks.MachineImageType.AMAZON_LINUX_2023,
+      ssmSessionPermissions: true,
     });
+
+    this.cluster.grantClusterAdmin('ArmAccess', asg.role.roleArn);
   }
 
   private assertFargateProfile() {
